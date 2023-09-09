85908f4c34a11e71aff61df50abf4e75165029d5
Josh <jks.hardy@gmail.com>
2023-09-09 21:19:45 +0530

    wip native token wrapper

--- rain/src/NativeTokenFlowCaller.sol
+++ rain/src/NativeTokenFlowCaller.sol
@@ -2,16 +2,19 @@
 pragma solidity ^0.8.18;
 
 import 'rain.flow/IFlowERC1155V3';
+import {WETH} from 'contracts/token/WETH.sol';
 
 contract NativeTokenFlowERC1155Caller {
     error ERC721TransferNotSupported();
     error ERC1155TransferNotSupported();
     error ERC20TransferNotSupported();
+    error WrongValueSent();
+    error CantBeTokenRecipient();
 
-    address public wrappedMaticAddress;
+    WETH public WMATIC;
 
-    function constructor(address _wrappedMaticAddress) public {
-        wrappedMaticAddress = _wrappedMaticAddress;
+    function constructor(WETH _WMATIC) public {
+        WMATIC = _WMATIC;
     }
 
     function flow(
@@ -20,25 +23,43 @@ contract NativeTokenFlowERC1155Caller {
         uint256[] calldata callerContext,
         SignedContextV1[] calldata signedContexts
     ) external payable public {
-        FlowERC1155IOV1 memory data = contract.previewFlow(evaluable, callerContext, signedContexts);
+
+        FlowERC1155IOV1 memory flowPreview = contract.previewFlow(evaluable, callerContext, signedContexts);
         uint256 wrappedMaticAmount = 0
 
-        if (data.flow.erc721.length > 0) {
+        // checks
+        if (flowPreview.flow.erc721.length > 0) {
             revert ERC721TransferNotSupported();
         }
-        if (data.flow.erc1155.length > 0) {
+        if (flowPreview.flow.erc1155.length > 0) {
             revert ERC1155TransferNotSupported();
         }
-        if (data.flow.erc20.length > 0) {
-            for (uint256 i = 0; i < data.flow.erc20.length; i++) {
-                if (data.flow.erc20[i].token == wrappedMaticAddress && data.flow.erc20[i].to == address(this) ) {
-                    wrappedMaticAmount += data.flow.erc20[i].amount;
+        if (flowPreview.flow.erc20.length > 0) {
+            for (uint256 i = 0; i < flowPreview.flow.erc20.length; i++) {
+                if (flowPreview.flow.erc20[i].token == address(WMATIC) && flowPreview.flow.erc20[i].to == address(this) ) {
+                    wrappedMaticAmount += flowPreview.flow.erc20[i].amount;
                 } else {
                     revert ERC20TransferNotSupported();
                 }
             }
         }
-
-
+        if (msg.value != wrappedMaticAmount) {
+            revert WrongValueSent();
+        }
+        if (flowPreview.mints.length > 0) {
+            for (uint256 i = 0; i < flowPreview.mints.length; i++) {
+                if (flowPreview.mints[i].account == address(this)) {
+                    revert CantBeTokenRecipient();
+                }
+            }
+        }
+        // wrap the matic
+        if (wrappedMaticAmount > 0) {
+            WMATIC.deposit(wrappedMaticAmount);
+        }
+        // approve the flow contract
+        WMATIC.approve(address(contract), wrappedMaticAmount);
+        
+        FlowERC1155IOV1 actualFlow = contract.flow(evaluable, callerContext, signedContexts);
     }
 }
